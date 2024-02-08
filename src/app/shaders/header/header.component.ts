f9de976f76c7a03ee95a0b6da4ede4ed3500f694
Egor Santamaria <esantamaria@vunkers.com>
2024-02-08 12:16:35 +0100

    Boto tancar sesió

--- src/app/shaders/header/header.component.ts
+++ src/app/shaders/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthSessionService } from 'src/app/services/AuthSessionService/auth-session-service.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor (private _authSessionService : AuthSessionService) {}
+
+  logOut (){
+    this._authSessionService.logout();
+  }
 
 }
